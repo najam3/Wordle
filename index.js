10030e736f88f409affdae0cca6a4a9a6f3b28fd
najam3 <100590374+najam3@users.noreply.github.com>
2023-07-30 01:23:16 +0500

    added game rules, settings.

--- index.js
+++ index.js
@@ -123,12 +123,29 @@ guesses[currentGuess].forEach((guess, i) => {
 });
 
 if( letter === 'ENTER' && positionTracker.length === 5 ) {
-  const originalWord = arr.join('');
-  const inputWord = input.join('');
+    const originalWord = arr.join('');
+    const inputWord = input.join('');
     const correctWord = arr.join('');
+  
+      const wordsGuessed = document.querySelector('.words-guessed');
+      const newWord = document.createElement('p');
+      if(originalWord === inputWord) {
+        newWord.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" color="green" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+      </svg>
+        ${inputWord}
+        `
+      }
+      newWord.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" color="red" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+      </svg>
+      ${inputWord}`;
+      wordsGuessed.appendChild(newWord)
      removeMouseEvent(originalWord, inputWord, position, correctWord);
      removeKeyEvent(originalWord, inputWord, position, correctWord);
-   
    input = [];
    guesses.push([]);
    currentGuess++;
@@ -139,11 +156,9 @@ if( letter === 'ENTER' && positionTracker.length === 5 ) {
 
 
 const removeMouseEvent = (word, guess, position, correctWord) => {
-     
+
    keys.forEach(key => {
-  
-    if(word === guess) 
-   
+   if(word === guess) 
        {
         key.removeEventListener('click', startGame);
         document.body.innerHTML = `
@@ -171,26 +186,33 @@ const removeMouseEvent = (word, guess, position, correctWord) => {
         </div>
     </div>
         `
-       }
+       } else if(word !== guess && position !== 25) {
+        key.removeEventListener('click', startGame)
+        setTimeout(() => {
+          key.addEventListener('click', startGame)
+        }, 3000)
+      }
   });
 }
 
 const removeKeyEvent = (word, guess, position, correctWord) => {
   if(word === guess) {
     window.removeEventListener('keydown', startWithKeyboard);
-    document.body.innerHTML = `
-      <div class="win-ui">
-      <div>
-          <h1>You've Won!</h1>
-          <p> You completed in ${position / 5} tries</p>
-          <div class="append-words"></div>
-          <video muted loop autoplay>
-              <source src="./animation_lko317f1.mp4" type="video/mp4">
-          </video>
-      </div>
-
-  </div>
-      `
+   setTimeout(() => {
+     document.body.innerHTML = `
+     <div class="win-ui">
+     <div>
+         <h1>You've Won!</h1>
+         <p> You completed in ${position / 5} tries</p>
+         <div class="append-words"></div>
+         <video muted loop autoplay>
+             <source src="./animation_lko317f1.mp4" type="video/mp4">
+         </video>
+     </div>
+ 
+ </div>
+     `
+   }, 3000)
   } else if(position === 25 && word !== guess) {
     window.removeEventListener('keydown', startWithKeyboard);
     document.body.innerHTML = `
@@ -205,10 +227,12 @@ const removeKeyEvent = (word, guess, position, correctWord) => {
 </div>
     `
 
+  }  else if(word !== guess && position !== 25) {
+    window.removeEventListener('keydown', startWithKeyboard)
+    setTimeout(() => {
+      window.addEventListener('keydown', startWithKeyboard)
+    }, 3000)
   }
-
-  //  word === guess ? window.removeEventListener('keydown', startWithKeyboard) : ''
-
 }
 
 
@@ -228,10 +252,6 @@ setTimeout(() => {
      }, 500);
    
    }, color.position  * 400);
-
-
-
-    
 
 
    keys.forEach((key) => {
@@ -254,6 +274,8 @@ const addAnimation = (letter) => {
       }, 200);
     } 
  })
+
+ 
 };
 
 
@@ -266,3 +288,13 @@ keys.forEach(key => {
 
 
 
+
+
+document.querySelector('.settings').addEventListener('click', () => {
+  const sidebar =   document.querySelector('.side_bar');
+    if(sidebar.classList.contains('open_sidebar')) {
+      sidebar.classList.remove('open_sidebar');
+    } else {
+      sidebar.classList.add('open_sidebar');
+    }  
+})
