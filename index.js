54cb004906d228e7dd25b795725da875996e1b00
najam3 <100590374+najam3@users.noreply.github.com>
2023-08-02 22:59:51 +0500

    Added settings

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
+// Simple words to be guessed...
 
 const words = ["FAIRY", "CROWN", "TRIED", "TOMBS"]
 
-const arr = words[Math.floor(Math.random() * words.length)].split("");
-console.log(arr)
+// Global Variables....
+let arr = words[Math.floor(Math.random() * words.length)].split("");
 let input = [];
 const result = document.querySelectorAll('#result');
 const keyboard = document.querySelector('.keyboard');
@@ -15,9 +16,11 @@ let position = 0;
 let positionTracker = [];
 let myArr = []
 
+
+  // Normal with Click
   const startGame = (e) => {
     const letter = e.target.innerText;
-    
+    // Check wether the click is a alphabat if not we return...
     if (/^[a-zA-Z]$/.test(letter)) {
       if(positionTracker.length === 5) return;
       input.push(letter);
@@ -27,6 +30,7 @@ let myArr = []
       console.log(position, positionTracker)
    }
    
+   // To cut a part of alphabat from the game area...
    if ( letter === 'BACKSPACE' && positionTracker.length !== 0 ) {
     positionTracker.pop();
     position--;
@@ -36,6 +40,7 @@ let myArr = []
   } 
   
   
+  // Simply find which letter in the box is correct...
    guesses[currentGuess].forEach(  (guess, i) => {
       result[position - 1].innerHTML = guess;
       if ( letter === 'ENTER' && positionTracker.length === 5 ) {  
@@ -54,13 +59,33 @@ let myArr = []
       }
    });
   
-  
+   // End here...
    if( letter === 'ENTER' && positionTracker.length === 5 ) {
     const originalWord = arr.join('');
     const inputWord = input.join('');
     const correctWord = arr.join('');
     removeMouseEvent(originalWord, inputWord, position, correctWord);
     removeKeyEvent(originalWord, inputWord, position, correctWord);
+    
+    // Append the guessed Words...
+    const wordsGuessed = document.querySelector('.words-guessed');
+    const newWord = document.createElement('p');
+    if(originalWord === inputWord) {
+      newWord.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" color="green" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+      <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+    </svg>
+      ${inputWord}
+      `
+    }
+    newWord.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" color="red" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+    </svg>
+    ${inputWord}`;
+
+    wordsGuessed.appendChild(newWord)
 
     input = [];
     guesses.push([]);
@@ -71,7 +96,7 @@ let myArr = []
 
 
 
-
+// Start with Key Events
 const startWithKeyboard = ({key}) => {
   const letter = key.toUpperCase();
 
@@ -102,10 +127,9 @@ guesses[currentGuess].forEach((guess, i) => {
    addAnimation(letter);
    
    if ( letter === 'ENTER' && positionTracker.length === 5 ) {  
-  
-    if ( arr[i] === guesses[currentGuess][i] ) {
-     const props = { index: positionTracker[i], bgColor: '#6BAA65', color: '#fff', txt: guess, position:  i === 4 ? 4 : i   };
-     addColorToBoard(props);     
+      if ( arr[i] === guesses[currentGuess][i] ) {
+      const props = { index: positionTracker[i], bgColor: '#6BAA65', color: '#fff', txt: guess, position:  i === 4 ? 4 : i   };
+       addColorToBoard(props);     
     }
 
     else if( arr.includes(guesses[currentGuess][i]) && arr[i] !== guesses[currentGuess][i]  ) {
@@ -123,9 +147,11 @@ guesses[currentGuess].forEach((guess, i) => {
 });
 
 if( letter === 'ENTER' && positionTracker.length === 5 ) {
-    const originalWord = arr.join('');
-    const inputWord = input.join('');
-    const correctWord = arr.join('');
+
+  
+  const originalWord = arr.join('');
+  const inputWord = input.join('');
+  const correctWord = arr.join('');
   
       const wordsGuessed = document.querySelector('.words-guessed');
       const newWord = document.createElement('p');
@@ -154,7 +180,7 @@ if( letter === 'ENTER' && positionTracker.length === 5 ) {
 }
 
 
-
+// Remove Event when the word is guessed or when all tries are taken...
 const removeMouseEvent = (word, guess, position, correctWord) => {
 
    keys.forEach(key => {
@@ -195,6 +221,8 @@ const removeMouseEvent = (word, guess, position, correctWord) => {
   });
 }
 
+
+// Remove Key Event simultaneously.... for same conditions...
 const removeKeyEvent = (word, guess, position, correctWord) => {
   if(word === guess) {
     window.removeEventListener('keydown', startWithKeyboard);
@@ -237,11 +265,9 @@ const removeKeyEvent = (word, guess, position, correctWord) => {
 
 
 
-
+// Add the Colors and other properties like styling when the user clicks Enter to guess his word....
 const addColorToBoard = (color) => {
-  
 setTimeout(() => {
-   
      result[color.index].parentElement.style.backgroundColor = color.bgColor;
      result[color.index].style.color = color.color;
      result[color.index].innerText = color.txt;
@@ -265,6 +291,8 @@ setTimeout(() => {
 
 }
 
+
+// Add animation simultaneously....
 const addAnimation = (letter) => {
   keys.forEach(key => {
     if(key.innerText === letter) {
@@ -274,22 +302,21 @@ const addAnimation = (letter) => {
       }, 200);
     } 
  })
-
- 
 };
 
 
 
-
+// Simply listen for key events on the window......
 window.addEventListener('keydown', startWithKeyboard);
+
+
+// Listen for mouse click events on the keyboard...
 keys.forEach(key => {
   key.addEventListener('click', startGame);
 });
 
 
-
-
-
+// Display the side_bar with its properites.....
 document.querySelector('.settings').addEventListener('click', () => {
   const sidebar =   document.querySelector('.side_bar');
     if(sidebar.classList.contains('open_sidebar')) {
@@ -298,3 +325,4 @@ document.querySelector('.settings').addEventListener('click', () => {
       sidebar.classList.add('open_sidebar');
     }  
 })
+
